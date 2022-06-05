6a783f24de81c45c35757a7eff6f450a07ae942f
Olliemint <furywrld5@gmail.com>
2022-06-05 18:15:09 +0300

    feat: sign up card

--- users/forms.py
+++ users/forms.py
@@ -5,16 +5,16 @@ from django.contrib.auth.models import User
 from django import forms
 
 
-class NewUserForm(UserCreationForm):
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'focus:outline-none','placeholder':'e.g example@gmail.com'}))
+    fullname = forms.CharField(required=True,widget=forms.TextInput(attrs={'class': 'focus:outline-none','placeholder':'e.g john'}))
     
     username = forms.CharField(required=True,widget=forms.TextInput(attrs={'class': 'focus:outline-none','placeholder':'e.g john'}))
-    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'focus:outline-none','placeholder':'e.g example@gmail.com'}))
     password1 = forms.CharField(required=True,widget=forms.PasswordInput(attrs={'class':'focus:outline-none'}))
-    password2 = forms.CharField(required=True,widget=forms.PasswordInput(attrs={'class':'focus:outline-none'}))
     
     class Meta:
         model = User
-        fields = ("username","email","password1","password2")
+        fields = ("email","fullname","username","password1")
     
     
 
