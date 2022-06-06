0c2e6876d83ccb6d9eec07b9767c3a2d5a684e82
Olliemint <furywrld5@gmail.com>
2022-06-06 11:48:20 +0300

    feat: class based view for login

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as authentication_views
 
 app_name = 'user'
 
 urlpatterns =[
     path('register/',views.register,name='register'),
+    path('login/',authentication_views.LoginView.as_view(template_name='users/login.html')),
+    path('logout/',authentication_views.LogoutView.as_view(template_name='users/logout.html')),
+
+
  
        
 ]
